0754c3e27e73631fa900fe3a80f701863a5f02e7
EDaniel0513 <39406566+EDaniel0513@users.noreply.github.com>
2020-04-07 22:18:02 -0400

    Update sketch.js
    small tweaks to the prompt box

--- HangMan/sketch.js
+++ HangMan/sketch.js
@@ -4,7 +4,7 @@
 
 */
 //promts the user for a phrase or dificalty
-var cOrV = prompt('Enter a: \n 1. To enter your own phrase, between 1 and 26 characters \n 2. For an easy word \n 3. for a moderate word \n 4. for a hard word \n 5. for a 612 inside joke').trim();
+var cOrV = prompt('Enter: \n 1. To enter your own phrase, between 1 and 26 characters \n 2. For an easy word \n 3. For a moderate word \n 4. For a hard word \n 5. For a 612 inside joke').trim();
 
 if (cOrV.length >= 1 && cOrV.toUpperCase() != cOrV.toLowerCase() ){
   var askUser = cOrV;
@@ -97,7 +97,7 @@ function draw() {
 
 //re-promts the user
 function startGame() {
- var cvp = prompt('Enter a: \n 1. To enter your own phrase, between 1 and 26 characters \n 2. For an easy word \n 3. for a moderate word \n 4. for a hard word \n 5. for a 612 inside joke').trim();
+ var cvp =prompt('Enter: \n 1. To enter your own phrase, between 1 and 26 characters \n 2. For an easy word \n 3. For a moderate word \n 4. For a hard word \n 5. For a 612 inside joke').trim();
   if (cvp.length > 1  || cvp.toUpperCase() != cvp.toLowerCase()) {
     nextPhrase = cvp;
     while (nextPhrase.length > 32 || nextPhrase.length < 1) {
